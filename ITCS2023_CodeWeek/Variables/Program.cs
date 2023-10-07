84b2a42b70901ec7a8df675c72810d120bb2cbe1
Mandy Farrugia <107984232+mandyfarrugia@users.noreply.github.com>
2023-10-07 09:43:46 +0200

    Created a new project Variables.
    Will be demonstrating different data types.

---
+++ ITCS2023_CodeWeek/Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ITCS2023_CodeWeek
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadLine(); //Leaves the output window until the Enter key is pressed.
+        }
+    }
+}
